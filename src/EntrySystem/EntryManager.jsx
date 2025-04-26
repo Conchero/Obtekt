c69848f93c162399336769b29340bb5dbed8926e
Conchero <florian.parra@hotmail.com>
2025-04-26 23:35:30 +0200

    Added React Functional Component

---
+++ src/EntrySystem/EntryManager.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const EntryManager = (props) => {
+  return (
+    <>
+      
+    </>
+  )
+};
+
+export default EntryManager;
---
+++ src/ObjectDetectionSystem/PredictionManagement.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PredictionManagement = (props) => {
+  return (
+    <>
+      
+    </>
+  )
+};
+
+export default PredictionManagement;
