05a336a2718d2ab92c6f398cad605e48046b5d2f
Hugo <hugo.thivel@gmail.com>
2025-04-26 19:57:07 +0200

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import WebcamCapture from './WebcamComponent'
-import ObjectDetectionTest from './ObjDetectionTest'
-import Home from './Pages/Home';
-import ObjectDetection from './Pages/ObjectDetection';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import WebcamCapture from "./WebcamComponent";
+import Home from "./Pages/Home";
+import ObjectDetection from "./Pages/ObjectDetection";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
-
-
-
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/object-detection" element={<ObjectDetection />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
