18de25f08f0750fb5e464f149fc7a55b97696ba4
Hugo <hugo.thivel@gmail.com>
2025-04-27 16:18:28 +0200

    added functionality to crop detected object's image for the saved entry screenshot

--- src/ObjectDetectionSystem/WebcamComponent.jsx
+++ src/ObjectDetectionSystem/WebcamComponent.jsx
@@ -7,24 +7,40 @@ import { MathBackendWebGL } from "@tensorflow/tfjs-backend-webgl";
 
 const WebcamCapture = ({ entry, setEntry, setEntries }) => {
   const webcamRef = useRef(null);
+  const [predictions, setPredictions] = useState([]);
 
   const getPrediction = async () => {
     const liveFeed = document.querySelector("video");
     const model = await cocoSsd.load();
-    const prediction = await model.detect(liveFeed);
+    const preds = await model.detect(liveFeed);
 
-    console.log("Prediction: ");
-    console.log(prediction);
+    setPredictions(preds);
+    console.log(preds);
   };
 
   /////////////////////////////////////////////////////////
   ////// Partie fictive pour faire marcher ma partie //////
   /////////////////////////////////////////////////////////
   const captureEntry = () => {
+    if (predictions.length === 0) {
+      alert("No prediction available");
+      return;
+    }
+
+    // Récupération de l'image croppée
+    const canvas = webcamRef.current.getCanvas();
+    const firstPrediction = predictions[0]; // Par contre je prends que le premier objet détecté, si y en a plusieurs toz
+    const { bbox, class: objectName, score } = firstPrediction;
+    const [x, y, width, height] = bbox;
+    const croppedCanvas = document.createElement("canvas");
+    croppedCanvas.width = width;
+    croppedCanvas.height = height;
+    const ctx = croppedCanvas.getContext("2d");
+    ctx.drawImage(canvas, x, y, width, height, 0, 0, width, height);
+
+    const screenshot = croppedCanvas.toDataURL("image/jpeg");
     const id = Math.random().toString(36).slice(2, 11);
-    const objectName = "Object";
-    const accuracyPercent = "97%";
-    const screenshot = webcamRef.current.getScreenshot();
+    const accuracyPercent = `${(score * 100).toFixed()}%`;
 
     setEntry({ id, objectName, accuracyPercent, screenshot });
   };
