498cd0bd7663b7fff8069bcddb21e3c159a4b06e
Conchero <florian.parra@hotmail.com>
2025-04-28 13:05:00 +0200

    qsd

--- src/Cards/DetectedEntryCard.jsx
+++ src/Cards/DetectedEntryCard.jsx
@@ -4,23 +4,18 @@ import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 
 
-const DetectedEntryCard = ({prediction,setEntry,removePredictionToShow} ) => {
+const DetectedEntryCard = ({ prediction, setEntry, removePredictionToShow }) => {
 
   const saveEntryToLocalStorage = () => {
     const existingEntries = JSON.parse(localStorage.getItem("entries")) || [];
-    console.log(existingEntries)
-    const updatedEntries = existingEntries.length > 0 ? [prediction,...existingEntries] : [prediction];
-    console.log(updatedEntries);
-    // const updatedEntries = [entry, ...existingEntries];
-     localStorage.setItem("entries", JSON.stringify(updatedEntries));
-     removePredictionToShow(prediction.id);
+    const updatedEntries = existingEntries.length > 0 ? [prediction, ...existingEntries] : [prediction];
+    localStorage.setItem("entries", JSON.stringify(updatedEntries));
+    removePredictionToShow(prediction.id);
     setEntry(prediction);
 
-     //setEntries(updatedEntries);
   };
 
   return (
-
     <div className="container flex bg-[#1B1B1B] justify-between items-center px-4 py-2 rounded-xl">
       <div className="info__containter flex items-center gap-2">
         <FontAwesomeIcon className="text-[#888888]" icon={faCircleInfo} />
@@ -31,11 +26,10 @@ const DetectedEntryCard = ({prediction,setEntry,removePredictionToShow} ) => {
       </div>
       <div className="user-choice__container flex gap-4">
         <button className="text-[20px] font-semibold bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[#00A150] p-2" onClick={saveEntryToLocalStorage}>Save it</button>
-        <button className="text-[20px] font-semibold  bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[ hover:text-[#353535] ]  p-2" onClick={(e) => {saveEntryToLocalStorage(prediction.id)}}>Discard</button>
+        <button className="text-[20px] font-semibold  bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[ hover:text-[#353535] ]  p-2" onClick={(e) => { saveEntryToLocalStorage(prediction.id) }}>Discard</button>
 
       </div>
     </div>
-
   );
 };
 
