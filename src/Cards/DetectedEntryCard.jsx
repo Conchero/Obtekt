e88251cee99c224e35e88d8c67d57574ea255948
Conchero <florian.parra@hotmail.com>
2025-04-28 12:37:11 +0200

    fix merge issue for card css fix

--- src/Cards/DetectedEntryCard.jsx
+++ src/Cards/DetectedEntryCard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+
 
 const DetectedEntryCard = ({ entry, setEntries }) => {
   
@@ -11,10 +14,19 @@ const DetectedEntryCard = ({ entry, setEntries }) => {
   };
 
   return (
-    <>
-      <p>DetectedEntryCard</p>
-      <button onClick={saveEntryToLocalStorage}>Save to LocalStorage</button>
-    </>
+    <div className="container flex bg-[#1B1B1B] justify-between items-center px-4 py-2 rounded-xl">
+      <div className="info__containter flex items-center gap-2">
+      <FontAwesomeIcon className="text-[#888888]" icon={faCircleInfo} />
+        <div className="flex-column">
+          <span className="text-[#888888] text-[16px] font-semibold">Object detected</span>
+          <h4 className="text-[#FFFFFF] text-[20px] font-bold">Lamp</h4>
+        </div>
+      </div>
+      <div className="user-choice__container flex gap-4">
+        <button className="text-[20px] font-semibold bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[#00A150] p-2 ">Save it</button>
+        <button className="text-[20px] font-semibold  bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[ hover:text-[#353535] ]  p-2">Discard</button>
+      </div>
+    </div>
   );
 };
 
--- src/Cards/SavedEntryCard.jsx
+++ src/Cards/SavedEntryCard.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const SavedEntryCard = ({ entry, deleteEntryFromLocalStorage }) => {
   return (
-    <div>
-      <a href={entry.screenshot} download={`screenshot-${entry.id}.jpg`}>
-        <img src={entry.screenshot} alt="screenshot" />
+    // <div>
+    //   <a href={entry.screenshot} download={`screenshot-${entry.id}.jpg`}>
+    //     <img src={entry.screenshot} alt="screenshot" />
+    //   </a>
+    //   <div>ID : {entry.id}</div>
+    //   <div>NAME : {entry.objectName}</div>
+    //   <div>ACCURACY : {entry.accuracyPercent}</div>
+    //   <button onClick={() => deleteEntryFromLocalStorage(entry.id)}>
+    //     Delete
+    //   </button>
+    // </div>
+
+    <div className="container flex bg-[#1B1B1B] text-[#888888] border">
+      <a  href={entry.screenshot} download={`screenshot-${entry.id}.jpg`}>
+      <div className={` border-red img__container bg-cover bg-center w-25 h-25 bg-stone-50`} style={{ backgroundImage: `url(${entry.screenshot})` }}>
+      </div>
       </a>
-      <div>ID : {entry.id}</div>
-      <div>NAME : {entry.objectName}</div>
-      <div>ACCURACY : {entry.accuracyPercent}</div>
-      <button onClick={() => deleteEntryFromLocalStorage(entry.id)}>
-        Delete
-      </button>
+      <div className=" px-4 ux__container w-[100%] border flex  items-center justify-between">
+      <div className="info__container">
+          <h3 className=" text-center rounded-2xl border-2 px-2 py-1 text-[12px]">{`${(entry.accuracyPercent*100).toFixed(0)}% accuracy`}</h3>
+          <div className="flex gap-2 items-center justify-evenly">
+            <h3 className="text-[#FFFFFF] text-[20px] font-bold">{entry.objectName}</h3>
+            <h3 className="text-[16px] font-semibold">{entry.id}</h3>
+          </div>
+        </div>
+      <FontAwesomeIcon className="hover:text-[#DB0004]" icon={faTrash} />
+      </div>
     </div>
   );
 };
