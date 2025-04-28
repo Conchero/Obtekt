d96b867636f938bf191747305be7030ad1612ede
Hugo <hugo.thivel@gmail.com>
2025-04-28 15:31:40 +0200

    css fix

--- src/Cards/DetectedEntryCard.jsx
+++ src/Cards/DetectedEntryCard.jsx
@@ -1,33 +1,52 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { MdInfoOutline, MdCheckCircleOutline } from "react-icons/md";
 
-
-
-const DetectedEntryCard = ({ prediction, setEntry, removePredictionToShow }) => {
-
+const DetectedEntryCard = ({
+  prediction,
+  setEntry,
+  removePredictionToShow,
+}) => {
   const saveEntryToLocalStorage = () => {
     const existingEntries = JSON.parse(localStorage.getItem("entries")) || [];
-    const updatedEntries = existingEntries.length > 0 ? [prediction, ...existingEntries] : [prediction];
+    const updatedEntries =
+      existingEntries.length > 0
+        ? [prediction, ...existingEntries]
+        : [prediction];
     localStorage.setItem("entries", JSON.stringify(updatedEntries));
     removePredictionToShow(prediction.id);
     setEntry(prediction);
-
   };
 
   return (
-    <div className="container flex bg-[#1B1B1B] justify-between items-center px-4 py-2 rounded-xl">
-      <div className="info__containter flex items-center gap-2">
-        <FontAwesomeIcon className="text-[#888888]" icon={faCircleInfo} />
+    <div className="container flex bg-[#1B1B1B]/75 backdrop-blur-md justify-between items-center px-[24px] py-[16px] rounded-xl">
+      <div className="info__containter flex items-center gap-[16px]">
+        <MdInfoOutline className="text-[#888888] text-[48px]" />
         <div className="flex-column">
-          <span className="text-[#888888] text-[16px] font-semibold">Object detected</span>
-          <h4 className="text-[#FFFFFF] text-[20px] font-bold">{prediction.objectName}</h4>
+          <span className="text-[#888888] text-[16px] font-semibold">
+            Object detected
+          </span>
+          <h4 className="text-[#FFFFFF] text-[20px] font-bold">
+            {prediction.objectName.charAt(0).toUpperCase() +
+              prediction.objectName.slice(1)}{" "}
+          </h4>
         </div>
       </div>
       <div className="user-choice__container flex gap-4">
-        <button className="text-[20px] font-semibold bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[#00A150] p-2" onClick={saveEntryToLocalStorage}>Save it</button>
-        <button className="text-[20px] font-semibold  bg-stone-50 rounded-md text-[#1B1B1B]  hover:text-[ hover:text-[#353535] ]  p-2" onClick={(e) => { saveEntryToLocalStorage(prediction.id) }}>Discard</button>
-
+        <button
+          className="flex flex-row items-center text-[20px] font-semibold bg-[#00A158] hover:bg-white rounded-[12px] text-white  hover:text-[#00A150] gap-[12px] px-[12px] py-[4px] group cursor-pointer transition"
+          onClick={saveEntryToLocalStorage}
+        >
+          <MdCheckCircleOutline className="group-hover:text-[#00A150] text-white text-[20px]" />
+          Save it
+        </button>
+        <button
+          className="text-[20px] font-semibold  bg-white rounded-[12px] hover:bg-[#1B1B1B] text-[#1B1B1B]  hover:text-[ hover:text-white transition p-2 cursor-pointer"
+          onClick={(e) => {
+            removePredictionToShow(prediction.id);
+          }}
+        >
+          Discard
+        </button>
       </div>
     </div>
   );
--- src/ObjectDetectionSystem/WebcamComponent.jsx
+++ src/ObjectDetectionSystem/WebcamComponent.jsx
@@ -2,24 +2,20 @@ import { useRef, useState, useEffect } from "react";
 import Webcam from "react-webcam";
 import DetectedEntryCard from "../Cards/DetectedEntryCard";
 
-
 import TimerComponent from "./TimerComponent";
 import PredictionManagement from "./PredictionManagement";
 
 const WebcamCapture = ({ entry, setEntry, setEntries }) => {
   const webcamRef = useRef(null);
 
-
   const [sendRequest, setSendRequest] = useState(false);
 
   const [predictionsToShow, setPredictionsToShow] = useState([]);
 
-
   const fillNewEntry = (_prediction) => {
     const newEntries = [];
 
-    _prediction.forEach(el => {
-
+    _prediction.forEach((el) => {
       const tempEntry = {
         id: Math.random().toString(36).slice(2, 11),
         objectName: el.class,
@@ -28,26 +24,23 @@ const WebcamCapture = ({ entry, setEntry, setEntries }) => {
       };
 
       newEntries.push(tempEntry);
-    })
+    });
 
     setPredictionsToShow(newEntries);
 
     return tempEntry;
-  }
+  };
 
-  useEffect(() => {
-  }, [])
-
+  useEffect(() => {}, []);
 
   const SendRequestToPredictionManagement = () => {
     setSendRequest(true);
-  }
+  };
 
   const RestartTimer = (_detectionState, _currentPrediction) => {
     setSendRequest(false);
 
     if (_currentPrediction != null) {
-
       switch (_detectionState) {
         case "SAME_DETECTION":
           break;
@@ -59,13 +52,11 @@ const WebcamCapture = ({ entry, setEntry, setEntries }) => {
           break;
       }
     }
-  }
-
+  };
 
   const removePredictionToShow = (id) => {
-    setPredictionsToShow(predictionsToShow.filter(el => el.id != id))
-  }
-
+    setPredictionsToShow(predictionsToShow.filter((el) => el.id != id));
+  };
 
   const captureEntry = () => {
     if (predictions.length === 0) {
@@ -108,9 +99,30 @@ const WebcamCapture = ({ entry, setEntry, setEntries }) => {
         <div className="absolute bottom-[64px] right-[64px] w-[192px] h-[192px] border-b-[8px] border-r-[8px] border-[#00A150]"></div>
       </div>
 
-      {(predictionsToShow != null && predictionsToShow.length) > 0 ? predictionsToShow.map(el => <DetectedEntryCard key={el.id} prediction={el} setEntry={setEntry} removePredictionToShow={removePredictionToShow} />): <></> }
-      <TimerComponent onTimerTriggerReached={SendRequestToPredictionManagement} requestPending={sendRequest} />
-      <PredictionManagement onRequestTreated={RestartTimer} requestAsked={sendRequest} />
+      <div className="absolute flex flex-col gap-[24px] bottom-[96px] w-full px-[96px]">
+        {(predictionsToShow != null && predictionsToShow.length) > 0 ? (
+          predictionsToShow.map((el) => (
+            <DetectedEntryCard
+              key={el.id}
+              prediction={el}
+              setEntry={setEntry}
+              removePredictionToShow={removePredictionToShow}
+            />
+          ))
+        ) : (
+          <></>
+        )}
+        <div className="hidden">
+          <TimerComponent
+            onTimerTriggerReached={SendRequestToPredictionManagement}
+            requestPending={sendRequest}
+          />
+          <PredictionManagement
+            onRequestTreated={RestartTimer}
+            requestAsked={sendRequest}
+          />
+        </div>
+      </div>
 
       <div className="absolute bottom-[96px]">
         {/* <button
@@ -126,9 +138,8 @@ const WebcamCapture = ({ entry, setEntry, setEntries }) => {
         >
           Capture infos
         </button> */}
-
       </div>
-{/* =======
+      {/* =======
 
 
   return (
@@ -140,7 +151,6 @@ const WebcamCapture = ({ entry, setEntry, setEntries }) => {
       <PredictionManagement onRequestTreated={RestartTimer} requestAsked={sendRequest} />
 >>>>>>> testnpm */}
     </div>
-
   );
 };
 
