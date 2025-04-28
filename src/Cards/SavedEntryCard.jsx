2010f974c73cc8155df21e9f3795c3d83a2a1a52
yasmina <y32781093@gmail.com>
2025-04-28 09:36:04 +0200

    add

--- src/Cards/SavedEntryCard.jsx
+++ src/Cards/SavedEntryCard.jsx
@@ -17,3 +17,42 @@ const SavedEntryCard = ({ entry, deleteEntryFromLocalStorage }) => {
 };
 
 export default SavedEntryCard;
+
+
+
+
+
+// import React from 'react';
+// import { FiTrash2 } from 'react-icons/fi'; // icône poubelle
+
+// const SavedEntryCard = ({ imageUrl, accuracy, name, id, onDelete }) => {
+//   return (
+//     <div className="bg-gray-800 p-3 rounded-lg flex items-center gap-4 mb-4">
+
+//       {/* Gauche : Image + Textes */}
+//       <div className="flex items-center gap-3 flex-1">
+        
+//         {/* Image */}
+//         <div className="w-14 h-14">
+//           <img src={imageUrl} alt={name} className="w-full h-full object-cover rounded" />
+//         </div>
+
+//         {/* Infos Texte */}
+//         <div className="flex flex-col">
+//           <span className="text-green-400 text-sm">{accuracy}% accuracy</span>
+//           <span className="text-white font-semibold">{name}</span>
+//           <span className="text-gray-400 text-xs">ID {id}</span>
+//         </div>
+
+//       </div>
+
+//       {/* Droite : bouton delete */}
+//       <button onClick={onDelete} className="text-red-500 hover:text-red-700">
+//         <FiTrash2 size={20} />
+//       </button>
+
+//     </div>
+//   );
+// };
+
+// export default SavedEntryCard;
